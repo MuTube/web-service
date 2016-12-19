7c635a462abd2bb7716390e2a8f9f15b3307473c
Luca Sardonini <luca.sardonini@liip.ch>
2016-12-19 10:03:53 +0100

    MWS-1 : Install swagger-ui, swagger-php and improve an API documentation

--- controller/apiCommandController.php
+++ controller/apiCommandController.php
@@ -1,5 +1,18 @@
 <?php
 
+
+/**
+ * Version 1 of the Mutube API
+ *
+ * @SWG\Info(
+ *     title="Commands",
+ *     version="1.0",
+ * )
+ *  @SWG\Swagger(
+ *     basePath="/api/run",
+ *     produces={"application/json"}
+ * )
+ */
 class ApiCommandController extends CommonCommandController {
 
     protected $httpMethod;
@@ -12,6 +25,13 @@ class ApiCommandController extends CommonCommandController {
         // HANDLE API DOCUMENTATION
 
         $this->setTemplate('api/doc.html.twig');
+    }
+
+    public function swaggerJson() {
+        $swagger = \Swagger\scan('controller/apiCommandController.php');
+        header('Content-Type: application/json');
+        echo $swagger;
+        exit();
     }
 
     public function run() {
@@ -54,6 +74,16 @@ class ApiCommandController extends CommonCommandController {
 
     // API Methods
 
+    /**
+     * @SWG\Get(
+     *   path="/track/request/{youtube_id}",
+     *   summary="Request a MP3 download for a given youtube id",
+     *   tags={"Track"},
+     *   @SWG\Parameter(name="youtube_id", type="string", required=true, in="path"),
+     *   @SWG\Response(response=200,description="Youtube MP3 download url"),
+     *   @SWG\Response(response=500,description="There was an error during the download, get error details")
+     * )
+     */
     protected function RequestTrack() {
         $youtubeId = $this->resourceId;
 
@@ -71,6 +101,16 @@ class ApiCommandController extends CommonCommandController {
         return($this->data['baseUrl'] . "/track/downloadMp3/" . $trackId);
     }
 
+    /**
+     * @SWG\Get(
+     *   path="/trackData/get/{youtube_id}",
+     *   summary="Get detail of a specific registered youtube id",
+     *   tags={"Track data"},
+     *   @SWG\Parameter(name="youtube_id", type="string", required=true, in="path"),
+     *   @SWG\Response(response=200,description="Youtube track data"),
+     *   @SWG\Response(response=404,description="Youtube id not found in DB")
+     * )
+     */
     protected function GetTrackData() {
         $youtubeId = $this->resourceId;
 
@@ -81,7 +121,7 @@ class ApiCommandController extends CommonCommandController {
         $trackData = TrackHistoryViewModel::getBy('youtube_id', $youtubeId);
 
         if($trackData == null) {
-            throw new Exception("Track with youtube id " . $youtubeId . " is not registered at the moment");
+            throw new Exception("Track with youtube id '" . $youtubeId . "' is not registered at the moment");
         }
 
         unset($trackData['id']);
@@ -93,6 +133,20 @@ class ApiCommandController extends CommonCommandController {
         return $trackData;
     }
 
+    /**
+     * @SWG\Post(
+     *   path="/trackData/push/{youtube_id}",
+     *   summary="Push track data for a given youtube id",
+     *   tags={"Track data"},
+     *   @SWG\Parameter(name="youtube_id", type="string", required=true, in="path"),
+     *   @SWG\Parameter(name="title", type="string", required=false, in="formData"),
+     *   @SWG\Parameter(name="artist", type="string", required=false, in="formData"),
+     *   @SWG\Parameter(name="album", type="string", required=false, in="formData"),
+     *   @SWG\Parameter(name="year", type="string", required=false, in="formData"),
+     *   @SWG\Response(response=200,description="Get push confirmation"),
+     *   @SWG\Response(response=404,description="Youtube id not found on Youtube")
+     * )
+     */
     protected function PushTrackData() {
         $youtubeId = $this->resourceId;
 
