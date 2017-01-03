0cac8c53d0950853e795c3663108a5f687119b25
Luca Sardonini <luca.sardonini@liip.ch>
2017-01-03 13:19:12 +0100

    Support PHP 5.5

--- tool/controller/sessionController.php
+++ tool/controller/sessionController.php
@@ -61,6 +61,19 @@ class SessionController {
     }
 
     public static function checkAuthentication($username, $password) {
+        if(!function_exists('hash_equals')) {
+            function hash_equals($str1, $str2) {
+                if(strlen($str1) != strlen($str2)) {
+                    return false;
+                } else {
+                    $res = $str1 ^ $str2;
+                    $ret = 0;
+                    for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
+                    return !$ret;
+                }
+            }
+        }
+
         if($user = UserViewModel::getBy('username', $username)) {
             if(hash_equals($user['password'], crypt($password, $user['password']))) {
                 return true;
